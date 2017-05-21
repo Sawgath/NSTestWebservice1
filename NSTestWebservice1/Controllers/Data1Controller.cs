549fc3bed567b195b51044d9fbea0df1a916801a
yamin05 <yams.stj@gmail.com>
2017-05-22 00:57:56 +1000

    bug fix with regex

--- NSTestWebservice1/Controllers/Data1Controller.cs
+++ NSTestWebservice1/Controllers/Data1Controller.cs
@@ -1,7 +1,4 @@
 ﻿using NSTestWebservice1.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
@@ -11,16 +8,11 @@ namespace NSTestWebservice1.Controllers
 {
     public class Data1Controller : ApiController
     {
-
-
-
         [HttpPost]
         public HttpResponseMessage Post(FetchData afUser)
         {
             string text = afUser.Username;
-            string pat = @"(\bor\b|\bselect\b|\bfrom\b|\binto\b|\bwhere\b|\binsert\b|\bupdate\b|\bdelete\b|
-                            \bdrop\b|\bcreate\b|\balter\b|\band\b|\bunion\b|\binner\b|\bouter\b|\bjoin\b|
-                            \-{2,}|\*|\\|\/|\=|\'|\`)";
+            string pat = @"(\bor\b|\bselect\b|\bfrom\b|\binto\b|\bwhere\b|\binsert\b|\bupdate\b|\bdelete\b|\bdrop\b|\bcreate\b|\balter\b|\band\b|\bunion\b|\binner\b|\bouter\b|\bjoin\b|\-{2,}|\*|\\|\/|\=|\'|\`)";
 
             // Instantiate the regular expression object.
             Regex r = new Regex(pat, RegexOptions.IgnoreCase);
@@ -32,12 +24,7 @@ namespace NSTestWebservice1.Controllers
                 return Request.CreateResponse(HttpStatusCode.Forbidden, "You tried to do sql Injection");
             }
             OtherFunctions aother = new OtherFunctions();
-
-
-
             return Request.CreateResponse(aother.GetAll(afUser.Username));
-
         }
-
     }
 }
